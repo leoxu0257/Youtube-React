6619dd2cca0e0be6e66ecd4dac53c6fd59c1dd40
leoxu0257 <enmacorradi@163.com>
2021-05-06 16:45:14 +1200

    add iframe

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import SearchBar from './SearchBar';
 import youtube from '../apis/youtube';
 import VideoList from './VideoList';
+import VideoDetail from './VideoDetail';
 
 
 class App extends React.Component{
@@ -21,17 +22,26 @@ class App extends React.Component{
 
     onVideoSelect= video =>{
         console.log('from the app',video);
+        this.setState({ selectedVideo: video });
     };
 
     render(){
         return( 
             <div className="ui container">
-                <SearchBar onFormSubmit={this.onTermSubmit} />
+                <SearchBar 
+                    onFormSubmit={this.onTermSubmit} 
+                />
+
                 I have {this.state.videos.length} videos.
+
+                <VideoDetail 
+                    video ={this.state.selectedVideo} 
+                />
+                
                 <VideoList 
                     onVideoSelect={this.onVideoSelect} 
                     videos={ this.state.videos}
-                ></VideoList>
+                />
             </div>
             
         );
