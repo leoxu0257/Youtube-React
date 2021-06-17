965fa95c921023955ad6128ff3fe8529cba9654e
leoxu0257 <enmacorradi@163.com>
2021-06-17 15:45:56 +1200

    add a custom hook

--- src/components/App.js
+++ src/components/App.js
@@ -5,23 +5,12 @@ import VideoList from './VideoList';
 import VideoDetail from './VideoDetail';
 
 const App = () => {
-    const [videos, setVideos] = useState([]);
     const [selectedVideo, setSelectedVideo] = useState(null);
 
-    useEffect(()=>{
-        onTermSubmit('food')
-    },[]);
+    
+    setSelectedVideo(response.data.items[0]);
 
-    const onTermSubmit = async term =>{
-        const response = await youtube.get('/search', {
-                params:{
-                    q:term
-                }
-            });
-
-            setVideos(response.data.items);
-            setSelectedVideo(response.data.items[0]);
-    };
+    
 
     return( 
         <div className="ui container">
